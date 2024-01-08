3ccb8481fb401933efccbcc8dac9cab805abc666
Arunava Gantait <arunavag@cmi.ac.in>
2024-01-08 19:39:55 +0530

    progressed in the nahas tutorial

--- my-programs/nahas_tutorial.v
+++ my-programs/nahas_tutorial.v
@@ -10,3 +10,25 @@ Proof.
   exact proof_of_A.
 Qed.
 
+(* Something a bit more complicated: *)
+
+Theorem forward_small : (forall A B : Prop, A -> (A -> B) -> B).
+Proof.
+  intros A.
+  intros B.
+  intros proof_of_A.
+  intros A_implies_B.
+  pose (proof_of_B := A_implies_B proof_of_A).  (* used in forward proofs *)
+  exact proof_of_B.
+Qed.
+
+(* Proof going backward *)
+
+Theorem backward_small : (forall A B : Prop, A -> (A -> B) -> B).
+Proof.
+  intros A B.
+  intros proof_of_A A_implies_B.
+  refine (A_implies_B _).
+  exact proof_of_A.
+Qed.
+
